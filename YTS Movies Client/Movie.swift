249818533d6918f443cc104c39f1f2e150de383f
Mahmoud Hosny <mahmoud.h.abdallah@gmail.com>
2017-12-17 18:48:01 +0200

    Empty application with dependancies.

---
+++ YTS Movies Client/Movie.swift
@@ -0,0 +1,23 @@
+//
+//  Movie.swift
+//  YTS Movies Client
+//
+//  Created by Mahmoud Hosny on 12/17/17.
+//  Copyright © 2017 Mahmoud Hosny. All rights reserved.
+//
+
+import Foundation
+
+class Movie{
+    
+    let id:Int
+    let backgroundImage:URL
+    let backGroundImageOriginal:URL
+    let dateUploaded:String
+    let descriptionFull:String
+    let language:String
+    let rating:Float
+    let titleEnglish:String
+    let year:String
+    let summary:String
+}
